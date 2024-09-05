43e700df0d387d1b05d336d8c435bfdca999d5b9
linuseing <linus@r13.de>
2024-09-06 00:15:04 +0200

    login singleplayer

--- src/waiting_single.cpp
+++ src/waiting_single.cpp
@@ -1,19 +1,42 @@
 #include "display.h"
 #include "game_state.h"
 
+#ifdef RFID
+#include "rfid.h"  
+#endif
+
 static bool updated_display = false;
 
 void waiting_single_loop() {
-    if (!updated_display) {
+    if (!updated_display && game_state.card_ids[0] == "") {
         display.clearDisplay();
         display.setTextSize(2);
         display.setTextColor(SSD1306_WHITE);
         display.setCursor(0,0);
-        display.println("CHUG!");
+        display.println("Start!");
+        display.println("not logged in");
         display.display();
 
         updated_display = true;
+    } else if (!updated_display) {
+        display.clearDisplay();
+        display.setTextSize(2);
+        display.setTextColor(SSD1306_WHITE);
+        display.setCursor(0, 0);
+        display.println("Start!");
+        display.println("logged in");
+        display.display();
+        updated_display = true;
     }
+
+    #ifdef RFID
+    String card_uid = read_uid();
+    if (card_uid == NULL) {
+        return;
+    }
+    game_state.card_ids[0] = card_uid;
+    updated_display = false;
+    #endif
 }
 
 void waiting_single_pad_lifted(int pad) {
